183473123df2ae7624b4565294e18a3f69fab60f
Andrey Panteleyev <insonifi@gmail.com>
2014-09-14 00:51:15 +0400

    overall update

--- webnode.js
+++ webnode.js
@@ -1,46 +1,31 @@
 var http = require('http'),
   iq = require('iq-node'),
   server = http.createServer(),
-  faye = require('faye'),
-  bayeux = new faye.NodeAdapter({mount: '/iq', timeout: 45}),
-  operator = bayeux.getClient(),
+  WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({port: 8080}),
   express = require('express'),
   app = express();
 
-app.use(express.static('app'))
+app.use(express.static('iq-webnode-app'))
 app.use(function(req, res, next){
-  res.send(404, 'Sorry cant find that!');
+  res.status(404).send('Sorry cant find that!');
 });
-bayeux.attach(server);
-server.listen(8080);
 app.listen(8000);
 
-operator.subscribe('/getconfig', function (req) {
-  req.prop = 'CONFIG';
-  iq.get(req).then(function (list) {
-    operator.publish('/config', list);
-  })
-});
-operator.subscribe('/getstate', function (req) {
-  req.prop = 'STATE';
-  iq.get(req).then(function (list) {
-    operator.publish('/state', list);
-  })
-});
-operator.subscribe('/event', function (msg) {
-  iq.sendEvent(msg);
-});
-operator.subscribe('/react', function (msg) {
-  iq.sendCoreReact(msg);
-});
-
-iq.connect({ip: '192.168.100.168', iidk: '1', host: 'GATE'});
-//iq.listen('iidk');
-iq.on({}, function (msg) {
-  if (msg.msg = 'Event') {
-    operator.publish('/channel', msg);
+wss.broadcast = function (message) {
+  var clients = Object.keys(this.clients),
+      i = clients.length;
+  for (; i--;) {
+    this.clients[clients[i]].send(message);
   }
-});
-iq.on({type: 'CAM', action: 'FRAME_SENT'}, function (msg) {
-  operator.publish('/frame', msg.params.data);
-})
+}
+
+
+iq.connect({ip: '192.168.122.183', iidk: '3', host: 'DUKE-PC'})
+.then(function () {
+  iq.on({}, function (msg) {
+    console.log(msg.type, msg.id, msg.action);
+    wss.broadcast(JSON.stringify(msg));
+  });
+}, function (e) { console.log(e); });
+//iq.listen('iidk');
