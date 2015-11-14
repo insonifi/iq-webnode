31eebeca4361cc62bd16b6345d00897037251fb7
Andrey Panteleyev <insonifi@gmail.com>
2015-11-14 16:40:15 +0300

    Dedicated module for various utility functions

--- iq-map/app/utils/misc.js
+++ iq-map/app/utils/misc.js
@@ -16,3 +16,15 @@ export function getState(type, id) {
     return {}
   }
 }
+export function boundedPosition(x, y) {
+  let bx = x;
+  let by = y;
+  var {w, h, s} = this;
+  var scaledWidth = w * s;
+  var scaledHeight = h * s;
+  bx = Math.min(bx, 0);
+  by = Math.min(by, 0);
+  bx = Math.max(bx, window.innerWidth - scaledWidth);
+  by = Math.max(by, window.innerHeight - scaledHeight);
+  return {bx, by};
+}
